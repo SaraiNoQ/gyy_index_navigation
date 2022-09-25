121daca8b9c3237268038c33ba593412eb9d1d35
SaraiNoQ <2994498974@qq.com>
2022-09-25 17:51:40 +0800

    fix: card image

--- src/views/home/components/StoreTest.vue
+++ src/views/home/components/StoreTest.vue
@@ -3,30 +3,34 @@ import Footer from "~/components/layout/Footer/index.vue";
 import Card from "~/components/layout/Card/index.vue"
 
 const infoArr = [{
-  url: 'http://www.epicq.top/images/gyy/%E5%A4%87%E6%80%A5%E5%8D%83%E9%87%91%E8%A6%81%E6%96%3F%E5%AE%8B%E6%A0%A1%E6%9C%3F.png',
-  title: '汉代医药典籍资源',
-  info: '本資料庫收錄了在甲骨、金文及簡牘中出現的各種詞彙，特色在於打破材質與時代的限制，以詞彙本身為焦點，提供簡便的查詢，以觀察詞彙的流變。'
+  url: '../../../assets/cover.png',
+  title: '宋校医书方剂文献异文图文语料库',
+  info: '本库收录宋校《伤寒论》《金匮要略》《金匮玉函经》《备急千金要方》《千金翼方》《外台秘要》6书方剂文献异文共计？处，以异文关联标注、电子文本与善本古籍图像互参的形式呈现。'
 }, {
-  url: 'http://www.epicq.top/images/gyy/%E5%A4%87%E6%80%A5%E5%8D%83%E9%87%91%E8%A6%81%E6%96%3F%E5%AE%8B%E6%A0%A1%E6%9C%3F.png',
-  title: '唐代医药典籍资源',
-  info: '殷周金文暨青銅器資料庫」收錄《殷周金文集成》、《新收殷周青銅器銘文暨器影彙編》，兩書共約14000件青銅器資料，完整呈現每件青銅器的背景訊息，供研究者使用（資料庫需申請帳號、密碼）。'
+  url: '../../../assets/cover.png',
+  title: '《千金要方》版本异文语料库',
+  info: '本库以通行本宋校《备急千金要方》为底本，以早期传本新雕本、日藏真本及英藏本、俄藏本为校本，汇集5种版本的异文约计?处。'
 }, {
-  url: 'http://www.epicq.top/images/gyy/%E5%A4%87%E6%80%A5%E5%8D%83%E9%87%91%E8%A6%81%E6%96%3F%E5%AE%8B%E6%A0%A1%E6%9C%3F.png',
-  title: '宋代医药典籍资源',
-  info: '本資料庫與台大藝術史研究所、倫敦大學亞非學院(SOAS)合作，進行銅器紋飾的分類工作，與「殷周金文暨青銅器資料庫」交互使用，更能全面了解青銅器。目前僅開放「紋飾分類樹狀圖」及「紋飾名稱權威檔」部份。'
+  url: '../../../assets/cover.png',
+  title: '《千金要方》宋校本与新雕本方剂异文语料库',
+  info: '本库汇集通行本宋校《备急千金要方》与早期传本《新雕孙真人千金方》方剂异文约计?处。'
 }, {
-  url: 'http://www.epicq.top/images/gyy/%E5%A4%87%E6%80%A5%E5%8D%83%E9%87%91%E8%A6%81%E6%96%3F%E5%AE%8B%E6%A0%A1%E6%9C%3F.png',
-  title: '明清医药典籍资源',
-  info: '本資料庫收錄了在甲骨、金文及簡牘中出現的各種詞彙，特色在於打破材質與時代的限制，以詞彙本身為焦點，提供簡便的查詢，以觀察詞彙的流變。'
+  url: '../../../assets/cover.png',
+  title: '《新雕孙真人千金方》与《千金翼方》方剂异文语料库',
+  info: '本库汇集《千金要方》早期传本《新雕孙真人千金方》与《千金翼方》方剂异文约计?处。'
 }, {
-  url: 'http://www.epicq.top/images/gyy/%E5%A4%87%E6%80%A5%E5%8D%83%E9%87%91%E8%A6%81%E6%96%3F%E5%AE%8B%E6%A0%A1%E6%9C%3F.png',
-  title: '其他医药典籍资源',
-  info: '本資料庫收錄了在甲骨、金文及簡牘中出現的各種詞彙，特色在於打破材質與時代的限制，以詞彙本身為焦點，提供簡便的查詢，以觀察詞彙的流變。'
+  url: '../../assets/cover.png',
+  title: '中医文献异文研究相关论文数据库',
+  info: '本库收录1949年至今中医文献异文研究相关学术论文，按照发表时间先后排序。'
+}, {
+  url: '~/assets/cover.png',
+  title: '求真中医善本古籍原文数据库',
+  info: '本库汇集《素问》《灵枢》《备急千金要方》《外台秘要》等二十余种中医善本古籍原文，提供了阅览、检索、征引等功能。'
 }]
 </script>
 
 <template>
-  <div class="flex flex-wrap gap-20 py-16 justify-center">
+  <div class="flex flex-wrap gap-20 mt-4 py-8 justify-center">
     <Card v-for="(item, index) in infoArr" :key="index" :url="item.url" :title="item.title" :info="item.info"  />
 
     <div v-if="infoArr.length%3 !== 0">
