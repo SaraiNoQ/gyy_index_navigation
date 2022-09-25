abf7e0d2e2f099ba97324d410dc0ae0d8078b72f
SaraiNoQ <2994498974@qq.com>
2022-09-25 17:52:09 +0800

    fix: card

--- src/components/layout/Card/index.vue
+++ src/components/layout/Card/index.vue
@@ -14,15 +14,18 @@ const redirectLink = (url?: string) => {
 </script>
   
   <template>
-    <div class="card w-72 glass">
-      <figure><img class="h-72 w-full" :src="url" alt="car!"></figure>
-      <div class="card-body">
-        <h2 class="card-title font-kai text-2xl">{{title}}</h2>
-        <p class="font-fang-song text-base">{{info}}</p>
-        <div class="card-actions justify">
-          <button class="btn btn-primary" @click="redirectLink(link)">相关资讯</button>
-          <button class="btn btn-primary">前往资料库 >></button>
+    <div class="card w-[320px] rounded-none">
+      <figure><img class="h-[237px] w-full" src="../../../assets/cover.png" alt="car!"></figure>
+      <div class="card-body mt-12 border-t-[3px]">
+        <!-- <h2 class="card-title font-kai text-2xl">{{title}}</h2> -->
+        <p class="font-fang-song text-sm leading-8 ml-[-34px] mt-[-24px] w-[316px] font-bold">{{info}}</p>
+        <div class="card-actions justify ml-[-34px]">
+          <button class="btn btn-primary bg-slate-100 border-slate-100 text-gray-500 hover:bg-slate-200 hover:border-slate-200" @click="redirectLink(link)">前往语料库 ></button>
         </div>
       </div>
     </div>
+    <!-- <div class="glass w-[320px]">
+      <figure><img class="h-[237px] w-full" :src="url" alt="car!"></figure>
+      <div class="">{{info}}</div>
+    </div> -->
   </template>
